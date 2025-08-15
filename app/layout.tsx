956c81d2f2766614e2efea1521bbd700894cbabf
conradholtzhausen <hollie@live.co.za>
2025-08-15 14:54:54 +0200

    Add CSQ script

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,38 +11,39 @@ import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "T-Shirt Store Yeah",
-  description: "Shop the latest t-shirt designs",
+	title: "T-Shirt Store Yeah",
+	description: "Shop the latest t-shirt designs",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <head>
-        <Script
-          id="inline-script"
-          dangerouslySetInnerHTML={{
-            __html: `
+	return (
+		<html lang="en">
+			<head>
+				<Script
+					id="inline-script"
+					dangerouslySetInnerHTML={{
+						__html: `
                window.heapReadyCb=window.heapReadyCb||[],window.heap=window.heap||[],heap.load=function(e,t){window.heap.envId=e,window.heap.clientConfig=t=t||{},window.heap.clientConfig.shouldFetchServerConfig=!1;var a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src="https://cdn.us.heap-api.com/config/"+e+"/heap_config.js";var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(a,r);var n=["init","startTracking","stopTracking","track","resetIdentity","identify","getSessionId","getUserId","getIdentity","addUserProperties","addEventProperties","removeEventProperty","clearEventProperties","addAccountProperties","addAdapter","addTransformer","addTransformerFn","onReady","addPageviewProperties","removePageviewProperty","clearPageviewProperties","trackPageview"],i=function(e){return function(){var t=Array.prototype.slice.call(arguments,0);window.heapReadyCb.push({name:e,fn:function(){heap[e]&&heap[e].apply(heap,t)}})}};for(var p=0;p<n.length;p++)heap[n[p]]=i(n[p])};
               heap.load("3605744716");
             `,
-          }}
-        />
-      </head>
-      <body className={inter.className}>
-        <PostHogProvider>
-          <AuthProvider>
-            <CartProvider>
-              <Header />
-              {children}
-            </CartProvider>
-          </AuthProvider>
-        </PostHogProvider>
-      </body>
-    </html>
-  );
+					}}
+				/>
+				<Script src="https://t.contentsquare.net/uxa/a1596f087dce6.js" />
+			</head>
+			<body className={inter.className}>
+				<PostHogProvider>
+					<AuthProvider>
+						<CartProvider>
+							<Header />
+							{children}
+						</CartProvider>
+					</AuthProvider>
+				</PostHogProvider>
+			</body>
+		</html>
+	);
 }
